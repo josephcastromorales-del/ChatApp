e3d277f1c07eb9a60653c6eca6a4da7945e95480
Joseph <josephcastromorales@gmail.com>
2025-11-29 19:35:00 -0500

    Delete main directory

--- main/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => {
-  console.log("Service Worker instalado");
-});
-
-self.addEventListener('fetch', (event) => {
-  // Permite navegar incluso offline si quieres agregar cache
-});
